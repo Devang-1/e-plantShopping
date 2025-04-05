a7bea2a167d7d8997a7105fe784a6fa3ec4a6803
Devang-1 <devang.shirode22@vit.edu>
2025-04-05 11:14:54 -0400

    initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,32 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], // Holds products added to cart
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const item = action.payload;
 
-    
+      const existingItem = state.items.find((i) => i.name === item.name);
+
+      if (!existingItem) {
+        state.items.push({ ...item, quantity: 1 });
+      } else {
+        existingItem.quantity += 1;
+      }
+    },
+
+    removeItem: (state, action) => {
+      const nameToRemove = action.payload;
+      state.items = state.items.filter(item => item.name !== nameToRemove);
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const item = state.items.find((i) => i.name === name);
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
